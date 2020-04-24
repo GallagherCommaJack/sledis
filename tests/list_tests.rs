0b66fc10fc2eb974f2cae548f3bf1ed0f25813f6
mobile-bungalow <pwmay@ucsc.edu>
2020-04-24 04:10:11 -0700

    some kludgey khaos tests

--- tests/list_tests.rs
+++ tests/list_tests.rs
@@ -36,38 +36,67 @@ impl Arbitrary for DequeuOp {
     }
 }
 
-fn deep_eq<S: ListStore, T: AsRef<[u8]>>(store: &S, deque: VecDeque<T>) -> bool {
-    true
+// assert a list and a dequeue have the same contents
+fn deep_eq<S: ListStore, T: AsRef<[u8]> + std::fmt::Debug>(
+    store: &S,
+    name: &[u8],
+    deque: &VecDeque<T>,
+) {
+    for idx in 0..store.list_len(name).ok().unwrap().unwrap() {
+        assert_eq!(
+            store.list_get(name, idx).ok().unwrap().unwrap(),
+            deque[idx as usize],
+            "Store and Dequeue did not have identical contents"
+        );
+    }
+}
+
+// assert the values popped from a std dequeue match that of a value popped
+// from a sledis list.
+fn pop_eq(deq_ret: Option<Vec<u8>>, sled_ret: Option<sled::IVec>) -> bool {
+    match (deq_ret, sled_ret) {
+        (None, None) => true,
+        (Some(d), Some(s)) => d.as_slice() == s.as_ref(),
+        _ => false,
+    }
 }
 
 quickcheck! {
 
     // test API identicallity to std::collections::VecDeque.
-    fn chaos_test_list(key: Vec<u8>, op_list: Vec<DequeuOp>) -> bool {
+    fn chaos_test_list(op_list: Vec<DequeuOp>) -> bool {
 
+        // init list store
         let name = b"chaos_test_list_name";
         let store = set_up_list_store();
         store.list_create(name).unwrap();
 
+        // init dequeue
         let mut dequeue = VecDeque::new();
+
         op_list.iter().for_each(|op| match op {
             DequeuOp::PushFront(val) => {
                 dequeue.push_front(val.clone());
                 store.list_push_front(name, val.clone()).unwrap();
+                deep_eq(&store, name, &dequeue);
             },
             DequeuOp::PushBack(val) => {
                 dequeue.push_back(val.clone());
                 store.list_push_back(name, val.clone()).unwrap();
+                deep_eq(&store, name, &dequeue);
             },
             DequeuOp::PopFront=> {
                 let deq_res = dequeue.pop_front();
                 let store_res = store.list_pop_front(name).unwrap();
+                assert!(pop_eq(deq_res, store_res), "PopFront returned a bad value.");
             },
             DequeuOp::PopBack => {
                 let deq_res = dequeue.pop_front();
                 let store_res = store.list_pop_front(name).unwrap();
+                assert!(pop_eq(deq_res, store_res), "PopBack returned a bad value.");
             },
         });
+
         true
     }
 
