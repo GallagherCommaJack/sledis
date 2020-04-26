cce9562c24da31605fc0c6986e1f18a8527fd198
theseriousadult <jack@kalix.io>
2020-04-26 03:11:54 -0700

    implement TableReadStore for ReadStore

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,8 @@ use thiserror::*;
 pub enum Error<E: std::error::Error> {
     #[error(transparent)]
     List(#[from] crate::list::Error),
+    #[error(transparent)]
+    Table(#[from] crate::table::Error),
     #[error("store error: {0}")]
     Store(E),
 }
--- src/table/mod.rs
+++ src/table/mod.rs
@@ -1,4 +1,5 @@
 use super::*;
+use thiserror::*;
 
 mod meta;
 pub use self::meta::*;
@@ -14,3 +15,41 @@ pub trait TableWriteStore: WriteStore {
     where
         IVec: From<V>;
 }
+
+impl<S> TableReadStore for S
+where
+    S: ReadStore,
+    S::Error: From<Error>,
+{
+    fn get_meta(&self, name: &[u8]) -> Result<Option<Meta>, Self::Error> {
+        let key = Key::Table { name, key: None }.encode();
+
+        if let Some(bs) = self.get(&key)? {
+            if let Some(got) = Meta::decode(Segment::new(bs)) {
+                Ok(Some(got))
+            } else {
+                Err(InvalidMeta(name.to_vec()).into())
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn table_get(&self, name: &[u8], key: &[u8]) -> Result<Option<IVec>, Self::Error> {
+        let key = Key::Table {
+            name,
+            key: Some(key),
+        }
+        .encode();
+
+        Ok(self.get(&key)?)
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("invalid table metadata, key was: {0:#?}")]
+    InvalidMeta(Vec<u8>),
+}
+
+use self::Error::*;
