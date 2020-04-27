58a42e397029636525b41dfdb88845c66bb0d27c
theseriousadult <jack@kalix.io>
2020-04-27 03:53:55 -0700

    add table_fetch_update

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -16,6 +16,16 @@ pub trait TableWriteStore: WriteStore {
         IVec: From<V>;
 
     fn table_remove(&self, name: &[u8], key: &[u8]) -> Result<Option<IVec>, Self::Error>;
+
+    fn table_fetch_update<V, F>(
+        &self,
+        name: &[u8],
+        key: &[u8],
+        f: F,
+    ) -> Result<Option<IVec>, Self::Error>
+    where
+        IVec: From<V>,
+        F: FnMut(Option<&[u8]>) -> Option<V>;
 }
 
 impl<S> TableReadStore for S
@@ -52,7 +62,7 @@ fn update_table_meta<S, F>(store: &S, name: &[u8], mut f: F) -> Result<Option<Me
 where
     S: WriteStore,
     S::Error: From<Error>,
-    F: FnMut(Option<Meta>) -> Result<Option<Meta>, S::Error>,
+    F: FnMut(Option<Meta>) -> Option<Meta>,
 {
     let key = Key::Table { name, key: None }.encode();
     let mut err: Option<S::Error> = None;
@@ -61,6 +71,7 @@ where
     store.fetch_update::<IVec, _>(&key, |iv| {
         let got = if let Some(bs) = iv {
             if let Some(got) = Meta::decode(Segment::new(bs.into())) {
+                err = None;
                 Some(got)
             } else {
                 err = Some(InvalidMeta(name.to_vec()).into());
@@ -70,18 +81,10 @@ where
             None
         };
 
-        match f(got) {
-            Ok(m) => {
-                err = None;
-                let res = m.as_ref().map(Meta::encode).map(IVec::from);
-                meta = m;
-                res
-            }
-            Err(e) => {
-                err = Some(e);
-                iv.map(IVec::from)
-            }
-        }
+        let new = f(got);
+        let res = new.as_ref().map(Meta::encode).map(IVec::from);
+        meta = new;
+        res
     })?;
 
     if let Some(e) = err {
@@ -115,7 +118,7 @@ where
                 meta.len += 1;
             }
 
-            Ok(Some(meta))
+            Some(meta)
         })?;
 
         Ok(old)
@@ -131,13 +134,69 @@ where
         let old = self.remove(&key)?;
 
         update_table_meta(self, name, |meta| {
-            Ok(meta.map(|mut meta| {
+            meta.map(|mut meta| {
                 if old.is_some() {
                     meta.len -= 1
                 }
                 meta
-            }))
+            })
         })?;
+
+        Ok(old)
+    }
+
+    fn table_fetch_update<V, F>(
+        &self,
+        name: &[u8],
+        key: &[u8],
+        mut f: F,
+    ) -> Result<Option<IVec>, Self::Error>
+    where
+        IVec: From<V>,
+        F: FnMut(Option<&[u8]>) -> Option<V>,
+    {
+        enum LenDiff {
+            Incr,
+            Decr,
+            None,
+        }
+        let mut ldiff = LenDiff::None;
+
+        let key = Key::Table {
+            name,
+            key: Some(key),
+        }
+        .encode();
+
+        let old = self.fetch_update(&key, |old| {
+            let new = f(old);
+            ldiff = match (&old, &new) {
+                (None, Some(_)) => LenDiff::Incr,
+                (Some(_), None) => LenDiff::Decr,
+                _ => LenDiff::None,
+            };
+            new
+        })?;
+
+        match ldiff {
+            LenDiff::Incr => {
+                update_table_meta(self, name, |meta| {
+                    meta.map(|mut meta| {
+                        meta.len += 1;
+                        meta
+                    })
+                })?;
+            }
+            LenDiff::Decr => {
+                update_table_meta(self, name, |meta| {
+                    meta.map(|mut meta| {
+                        meta.len -= 1;
+                        meta
+                    })
+                })?;
+            }
+            LenDiff::None => {}
+        }
 
         Ok(old)
     }
